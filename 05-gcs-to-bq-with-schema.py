784fef25663bed57f3f6d551431c78479a44867e
siriyaparn <siriyasriboonma@gmail.com>
2024-06-24 01:21:12 +0700

    initial commit

---
+++ 05-gcs-to-bq-with-schema.py
@@ -0,0 +1,202 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.dates import days_ago
+import pandas as pd
+import requests
+
+MYSQL_CONNECTION = "mysql_default"      # connection name created in Airflow
+CONVERSION_RATE_URL = ""                # Set URL
+
+# Set path
+mysql_output_path = "/home/airflow/gcs/data/audible_data_merged.csv"
+conversion_rate_output_path = "/home/airflow/gcs/data/conversion_rate.csv"
+final_output_path = "/home/airflow/gcs/data/output.csv"
+
+# Create function
+def get_data_from_mysql(transaction_path):
+    # Get the transaction_path from the running task
+    # Call MySqlHook to connect to MySQL from the connection created in Airflow
+    mysqlserver = MySqlHook(MYSQL_CONNECTION)
+    
+    # Query from database using Hook
+    # Results pandas DataFrame
+    audible_data = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_data")
+    audible_transaction = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_transaction")
+
+    # Merge data: audible_data, audible_transaction
+    df = audible_transaction.merge(audible_data, how="left", left_on="book_id", right_on="Book_ID")
+
+    # Save CSV file to transaction_path ("/home/airflow/gcs/data/audible_data_merged.csv")
+    df.to_csv(transaction_path, index=False)
+    print(f"Output to {transaction_path}")
+
+
+def get_conversion_rate(conversion_rate_path):
+    r = requests.get(CONVERSION_RATE_URL)
+    result_conversion_rate = r.json()
+    df = pd.DataFrame(result_conversion_rate)
+
+    # Change the index that is date to a column named date instead and save the csv file.
+    df = df.reset_index().rename(columns={"index": "date"})
+    df.to_csv(conversion_rate_path, index=False)
+    print(f"Output to {conversion_rate_path}")
+
+
+def merge_data(transaction_path, conversion_rate_path, output_path):
+    # Read csv file from transaction_path
+    transaction = pd.read_csv(transaction_path)
+    conversion_rate = pd.read_csv(conversion_rate_path)
+
+    transaction['date'] = transaction['timestamp']
+    transaction['date'] = pd.to_datetime(transaction['date']).dt.date
+    conversion_rate['date'] = pd.to_datetime(conversion_rate['date']).dt.date
+
+    # merge 2 DataFrame
+    final_df = transaction.merge(conversion_rate, how="left", left_on="date", right_on="date")
+    
+    # Convert the price by removing the $ sign and converting to float
+    final_df["Price"] = final_df.apply(lambda x: x["Price"].replace("$",""), axis=1)
+    final_df["Price"] = final_df["Price"].astype(float)
+
+    final_df["THBPrice"] = final_df["Price"] * final_df["conversion_rate"]
+    final_df = final_df.drop(["date", "book_id"], axis=1)
+
+    # Save csv file
+    final_df.to_csv(output_path, index=False)
+    print(f"Output to {output_path}")
+    print("== End of Workshop 4 ʕ•́ᴥ•̀ʔっ♡ ==")
+
+# Define the DAG
+with DAG(
+    "workshop5_gcs_to_bq_dag",
+    start_date=days_ago(1),
+    schedule_interval="@once",
+    tags=["workshop"]
+) as dag:
+
+    dag.doc_md = """
+    # Load to BigQuery with GCSToBigQueryOperator command from Airflow 
+    Suitable for workers which do not have bq command
+    Documentation : https://airflow.apache.org/docs/apache-airflow-providers-google/stable/operators/cloud/gcs.html
+    Example: https://airflow.apache.org/docs/apache-airflow-providers-google/stable/_modules/airflow/providers/google/cloud/example_dags/example_gcs_to_bigquery.html
+    """
+
+    # Create tasks
+    t1 = PythonOperator(
+        task_id="get_data_from_mysql",
+        python_callable=get_data_from_mysql,
+        op_kwargs={"transaction_path": mysql_output_path},
+    )
+
+    t2 = PythonOperator(
+        task_id="get_conversion_rate",
+        python_callable=get_conversion_rate,
+        op_kwargs={"conversion_rate_path": conversion_rate_output_path},
+    )
+
+    t3 = PythonOperator(
+        task_id="merge_data",
+        python_callable=merge_data,
+        op_kwargs={
+            "transaction_path": mysql_output_path,
+            "conversion_rate_path": conversion_rate_output_path, 
+            "output_path": final_output_path
+        },
+    )
+
+    t4 = GCSToBigQueryOperator(
+        task_id='gcs_to_bq',
+        bucket='asia-east2-workshop5-03d65f08-bucket',
+        source_objects=['data/output.csv'],
+        destination_project_dataset_table='workshop5.audible_data2',
+        skip_leading_rows=1,
+        schema_fields=[
+            {
+                "mode": "NULLABLE",
+                "name": "timestamp",
+                "type": "TIMESTAMP"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "user_id",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "country",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_ID",
+                "type": "INTEGER"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Title",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Subtitle",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Author",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Narrator",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Audio_Runtime",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Audiobook_Type",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Categories",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Rating",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Total_No__of_Ratings",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Price",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "conversion_rate",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "THBPrice",
+                "type": "FLOAT"
+            }
+        ],
+        write_disposition='WRITE_TRUNCATE',
+    )
+
+    # Set the task dependencies
+    [t1, t2] >> t3 >> t4
